6135336cb1741c40b6a69296b4e670c7927e977f
Ihor <kovaluk_igor@ukr.net>
2024-04-07 21:00:38 +0200

    Create AppBar.js

---
+++ src/components/AppBar/AppBar.js
@@ -0,0 +1,35 @@
+import { Navigation } from 'components/Navigation/Navigation';
+import { UserMenu } from 'components/UserMenu/UserMenu';
+import { AuthNav } from 'components/AuthNav/AuthNav';
+import { useAuth } from 'components/hooks/useAuth';
+
+
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+
+export const ButtonAppBar = () => {
+  const { isLoggedIn } = useAuth();
+
+  return (
+    <div>
+      <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <Navigation />
+          </Typography>
+          {isLoggedIn ? <UserMenu /> : <AuthNav />}
+        </Toolbar>
+      </AppBar>
+    </Box>
+
+    </div>
+     
+
+
+  );
+};
