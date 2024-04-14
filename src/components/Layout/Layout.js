ad1f5a1aae6cd26e82a529d27864cc75494f8cc0
Ihor <kovaluk_igor@ukr.net>
2024-04-14 15:04:59 +0200

    Update Layout.js

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,25 @@
 import { Outlet } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
+
 
 import { Suspense } from 'react';
 import { ButtonAppBar } from 'components/AppBar/AppBar';
 
+import css from './Layout.module.css'
+
 export const Layout = () => {
   return (
-    <div >
+    <div>
+      <div className={css.layout}>
       <ButtonAppBar />
+      </div>
+      
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>
-      <Toaster position="top-right" reverseOrder={false} />
+      
+
     </div>
+    
+    
   );
 };
