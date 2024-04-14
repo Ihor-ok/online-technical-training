96770aca90ed17575a5552876e36b4eaca6e58e6
Ihor <kovaluk_igor@ukr.net>
2024-04-14 15:05:28 +0200

    Update UserMenu.js

--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -3,6 +3,9 @@ import { useDispatch } from 'react-redux';
 import { useAuth } from 'components/hooks/useAuth';
 import { logOut } from 'components/redux/auth/operations';
 
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
 
 
 export const UserMenu = () => {
@@ -13,14 +16,26 @@ export const UserMenu = () => {
 
 
   return (
-    <div >
+    <div style={{
+      display: 'flex',
+      alignItems: 'center',
+      gap: 12,
+    }}>
       <p >Welcome, <b>{user.name}</b>!</p>
-      <button type="button" onClick={() => {
+      <Stack spacing={2} direction="row">
+        <Button variant="contained" onClick={() => {
+      
+          dispatch(logOut())
+          }}>
+          Logout
+        </Button>
+      </Stack>
+      {/* <button type="button" onClick={() => {
       
         dispatch(logOut())
       }}>
         Logout
-      </button>
+      </button> */}
     </div>
   );
 };
