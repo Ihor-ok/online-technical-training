7db06b6a28a48e1a55a61d5674ac4278ad46c917
Ihor <kovaluk_igor@ukr.net>
2024-04-14 15:05:18 +0200

    Update Navigation.js

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,20 +1,31 @@
 import { useAuth } from "components/hooks/useAuth";
 import { NavLink } from "react-router-dom";
 
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+
+
+
 export const Navigation = () => {
 const { isLoggedIn } = useAuth();
   return (
     <nav>
-      <NavLink to="/">
-        Home
-      </NavLink>
+    <Stack spacing={2} direction="row">
+        
+          <NavLink to="/">
+            <Button variant="contained" >Home</Button>  
+          </NavLink>
 
-      {isLoggedIn && (
-        <NavLink to="/courses">
-          Courses
-        </NavLink>
-      )}
-      
+        {isLoggedIn && (
+            <NavLink to="/courses">
+              <Button variant="contained" >Courses</Button>
+            </NavLink>
+        )}
+    </Stack>
+
+     
+            
+
 
     </nav>
   );
