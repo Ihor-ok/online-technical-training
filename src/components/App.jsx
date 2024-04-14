7551484f9ccf1690a280bbe001e0ea763b355b0e
Ihor <kovaluk_igor@ukr.net>
2024-04-14 15:04:20 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,6 +9,15 @@ import { Layout } from "./Layout/Layout";
 import { RestrictedRoute } from "RestrictedRoute";
 import { PrivateRoute } from "PrivateRoute";
 
+import css from './App.module.css'
+import Training148 from "./Training147/Training147";
+import MaintenancePrograms from "./MaintenancePrograms/MaintenancePrograms";
+import MilitaryAviation from "./MilitaryAviation/MilitaryAviation";
+import AviationLegislation from "./AviationLegislation/AviationLegislation";
+import ElectricalWiring from "./ElectricalWiring/ElectricalWiring";
+import BatteryMaintenance from "./BatteryMaintenance/BatteryMaintenance";
+import Qrosscheck from "./Qrosscheck/Qrosscheck";
+
 
 
 
@@ -28,10 +37,16 @@ export const App = () => {
      
 
     return isRefreshing ? (
-    <b>Refreshing user...</b>
+      <div className={css.App}>
+         <b>Refreshing user...</b>
+      </div>
+     
   ) : (
-              <div>
-            <Routes>
+        <div className={css.App}>
+
+        
+        <Routes>
+          
                 <Route path="/" element={<Layout />}>
                     <Route index element={<HomePage />} />
                     <Route
@@ -52,8 +67,52 @@ export const App = () => {
                      <PrivateRoute redirectTo="/" component={<CoursesPage />} />
                 }
                 />
+                <Route
+                path="/courses/training148"
+                element={
+                     <PrivateRoute redirectTo="/" component={<Training148 />} />
+                }
+                />
+                <Route
+                path="/courses/maintenance_programs"
+                element={
+                     <PrivateRoute redirectTo="/" component={<MaintenancePrograms />} />
+                }
+                />
+                <Route
+                path="/courses/military_aviation_legislation"
+                element={
+                     <PrivateRoute redirectTo="/" component={<MilitaryAviation />} />
+                }
+                />
+                <Route
+                path="/courses/aviation_legislation"
+                element={
+                     <PrivateRoute redirectTo="/" component={<AviationLegislation />} />
+                }
+                />
+                <Route
+                path="/courses/electrical_wiring"
+                element={
+                     <PrivateRoute redirectTo="/" component={<ElectricalWiring />} />
+                }
+                />
+                <Route
+                path="/courses/battery_maintenance"
+                element={
+                     <PrivateRoute redirectTo="/" component={<BatteryMaintenance />} />
+                }
+                />
+                <Route
+                path="/courses/qrosscheck"
+                element={
+                     <PrivateRoute redirectTo="/" component={<Qrosscheck />} />
+                }
+                />
+                
                 </Route>
         </Routes>
-        </div>
+        
+      </div>
   )
 };
