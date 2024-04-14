957d6537ee09d838e85935599e0dd0eeb3a6cdc8
Ihor <kovaluk_igor@ukr.net>
2024-04-14 15:06:03 +0200

    Update Courses.js

--- src/pages/Courses.js
+++ src/pages/Courses.js
@@ -1,9 +1,44 @@
+import Card147Training from "components/CoursesCards/Card147Training";
+import CardAviationLegislation from "components/CoursesCards/CardAviationLegislation";
+import CardMaintenancePrograms from "components/CoursesCards/CardMaintenancePrograms";
+import CardMilitaryAviation from "components/CoursesCards/CardMilitaryAviation";
+import CardElectricalWiring from "components/CoursesCards/CardElectricalWiring";
+import CardBatteryMaintenance from "components/CoursesCards/CardBatteryMaintenance";
+import CardQrosscheck from "components/CoursesCards/CardQrosscheck";
+import { motion } from "framer-motion";
+
+
 export default function Courses() {
   return (
-    <div>
-      <h1>
-        Courses kard
-      </h1>
-    </div>
-  );
+  
+    <motion.div
+      style={{
+      display: 'flex',
+        flexWrap: 'wrap', justifyContent: 'center'
+      }}
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{
+        duration: 0.8,
+        delay: 0.5,
+        ease: [0, 0.71, 0.2, 1.01]
+      }}
+    >
+
+        <Card147Training />
+
+        <CardMaintenancePrograms />
+
+         <CardMilitaryAviation />
+
+        <CardAviationLegislation />
+
+        <CardElectricalWiring />
+
+         <CardBatteryMaintenance />
+
+        <CardQrosscheck />
+     
+    </motion.div>
+ )
 }
