812891da7af70165eaa637e7198191ff0e9a75d2
Ihor <kovaluk_igor@ukr.net>
2024-04-14 22:54:15 +0200

    Update Modal.js

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Modal from '@mui/material/Modal';
 import ReactPlayer from 'react-player';
-import { ModalClose } from '@mui/joy';
+
 
 const VideoModal = ({ open, onClose, url }) => {
     console.log(open);
