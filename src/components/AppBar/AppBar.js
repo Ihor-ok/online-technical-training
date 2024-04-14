11ff596cba6644a40fc5993702c601ad33e1e713
Ihor <kovaluk_igor@ukr.net>
2024-04-14 15:04:25 +0200

    Update AppBar.js

--- src/components/AppBar/AppBar.js
+++ src/components/AppBar/AppBar.js
@@ -9,25 +9,48 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+import css from './AppBar.module.css'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#2e7d32',
+      light: '#4caf50',
+      dark: '#1b5e20',
+      contrastText: '#fff',
+    },
+  },
+});
+
+
 
 
 export const ButtonAppBar = () => {
   const { isLoggedIn } = useAuth();
 
   return (
-    <div>
+    
+      <ThemeProvider theme={theme}>
       <Box sx={{ flexGrow: 1 }}>
-      <AppBar color="secondary" position="static">
-        <Toolbar>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Navigation />
-          </Typography>
-          {isLoggedIn ? <UserMenu /> : <AuthNav />}
-        </Toolbar>
-      </AppBar>
+        
+          <AppBar position="static">
+            <div className={css.AppBar}>
+              <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                <Navigation />
+              </Typography>
+              {isLoggedIn ? <UserMenu /> : <AuthNav />}
+              </Toolbar>
+          </div>
+          </AppBar>
+        
     </Box>
+      </ThemeProvider>
+      
 
-    </div>
+    
      
 
 
