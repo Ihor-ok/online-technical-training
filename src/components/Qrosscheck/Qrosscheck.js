90001deec1568e73d83a4242201a5441b0f76ca7
Ihor <kovaluk_igor@ukr.net>
2024-04-14 15:05:21 +0200

    Create Qrosscheck.js

---
+++ src/components/Qrosscheck/Qrosscheck.js
@@ -0,0 +1,11 @@
+export default function Qrosscheck () {
+  return (
+    <div>
+      <ul>
+        <li>Compliance Monitoring System Course - EASA</li>
+        <li>MEL - Minimum Equipment List Course (EASA & UK)</li>
+        <li>SMS – Safety Management System Course</li>
+      </ul>
+    </div>
+  );
+}
