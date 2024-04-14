5d5c530b60e123737ac9955a245dcd6daac20d67
Ihor <kovaluk_igor@ukr.net>
2024-04-14 15:05:07 +0200

    Create MaintenancePrograms.js

---
+++ src/components/MaintenancePrograms/MaintenancePrograms.js
@@ -0,0 +1,12 @@
+export default function MaintenancePrograms() {
+  return (
+    <div>
+      <ul>
+        <li>Welcome</li>
+        <li>Maintenance Programs</li>
+        <li>Reliability Programs</li>
+        <li>Maintenance Costs</li>
+      </ul>
+    </div>
+  );
+}
