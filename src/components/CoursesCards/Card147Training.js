12616a8b1c24273db540d45d3e93d80542f82f85
Ihor <kovaluk_igor@ukr.net>
2024-04-19 11:32:27 +0200

    Update Card147Training.js

--- src/components/CoursesCards/Card147Training.js
+++ src/components/CoursesCards/Card147Training.js
@@ -6,15 +6,15 @@ import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
 
 import img from '../../img/147Training.jpg'
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export default function Card147Training() {
 
-
+const location = useLocation();
   return (
     
     <Card sx={{position: 'relative', maxWidth: '365px', minHeight: '450px', margin: '10px', display: 'flex', flexDirection: 'column', justifyContent: 'space-between' }} >
-      <Link to={`/courses/training148`} style={{ textDecoration: 'none', color: 'inherit', flexGrow: 1 }}>
+      <Link to={`/courses/training148`} state={location} style={{ textDecoration: 'none', color: 'inherit', flexGrow: 1 }}>
       <CardActionArea>
         <CardMedia
           component="img"
