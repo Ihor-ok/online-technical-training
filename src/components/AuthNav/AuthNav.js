3fef78fd60cf6ea3ae29edc7fc80cd809ea23824
Ihor <kovaluk_igor@ukr.net>
2024-04-20 23:17:49 +0200

    Update AuthNav.js

--- src/components/AuthNav/AuthNav.js
+++ src/components/AuthNav/AuthNav.js
@@ -2,13 +2,21 @@ import { NavLink } from 'react-router-dom';
 import Button from '@mui/material/Button';
 
 
-export const AuthNav = () => {
+export const AuthNav = (props) => {
   return (
     <div>
       <NavLink  to="/register">
-        <Button  style={{ marginRight: 10 }}  variant="contained">Register</Button>
+        <Button
+          style={{ marginRight: 10 }}
+          variant="contained"
+          onClick={()=>{props.handleButtonClick('courses')}}
+        >Register</Button>
       </NavLink>
-      <NavLink to="/login"><Button  variant="contained"> Log In</Button>
+      <NavLink
+          to="/login"><Button
+          variant="contained"
+          onClick={()=>{props.handleButtonClick('courses')}}
+        > Log In</Button>
       </NavLink>
 
      
