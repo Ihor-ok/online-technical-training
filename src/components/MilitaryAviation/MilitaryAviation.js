6e1930644b40629b2a19721c0a92dbcc6bbe4012
Ihor <kovaluk_igor@ukr.net>
2024-04-22 16:44:54 +0200

    Update MilitaryAviation.js

--- src/components/MilitaryAviation/MilitaryAviation.js
+++ src/components/MilitaryAviation/MilitaryAviation.js
@@ -1,6 +1,39 @@
+import { useRef, useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+
 export default function MilitaryAviation() {
+
+  const [isHovered, setIsHovered] = useState(false);
+  const location = useLocation();
+  const backLinkLocationRef = useRef(location);
+
+  const btnGoBackStyle = {
+    marginLeft: '10px',
+    marginTop:'10px',
+    backgroundColor: '#FF4500',
+      color: '#FFFFFF',
+      border: 'none',
+      padding: '5px 10px',
+      fontSize: '16px',
+      cursor: 'pointer',
+      borderRadius: '5px',
+      transition: 'background-color 0.3s, color 0.3s',
+      ...(isHovered && { backgroundColor: '	#006400', color: '#FFFFFF' }),
+    };
+
+
   return (
     <div>
+      <Link to={backLinkLocationRef.current.state}>
+        <button
+          style={btnGoBackStyle}
+          onMouseEnter={() => setIsHovered(true)}
+          onMouseLeave={() => setIsHovered(false)}
+        >
+           Go back
+        </button>
+      </Link>
+
       <ul>
         <li>Military Aviation Legislation</li>
       </ul>
